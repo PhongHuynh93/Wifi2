134c3fa8a5a808fdd6691c79e83bc4c46cf0f356
Phong <p7157568@gmail.com>
2016-06-19 15:54:32 +0700

    feat - add image but image height wrong

--- app/src/main/java/dhbk/android/wifi2/utils/ImageAnimator.java
+++ app/src/main/java/dhbk/android/wifi2/utils/ImageAnimator.java
@@ -7,6 +7,7 @@ import android.widget.ImageView;
 import dhbk.android.wifi2.adapters.historyAdapters.HistoryPagerAdapter;
 
 class ImageAnimator {
+    private static final float FACTOR = 0.1f;
     private final HistoryPagerAdapter pagerAdapter;
     private final ImageView outgoing;
     private final ImageView imageView;
@@ -29,6 +30,7 @@ class ImageAnimator {
         @DrawableRes int incomingId = pagerAdapter.getImageId(finalPosition);
         positionFactor = 1f / (end - start);
         outgoing.setImageDrawable(imageView.getDrawable());
+        outgoing.setTranslationX(0f);
         outgoing.setVisibility(View.VISIBLE);
         outgoing.setAlpha(1f);
         imageView.setImageResource(incomingId);
@@ -49,13 +51,20 @@ class ImageAnimator {
 
     public void forward(int position, float positionOffset) {
         float offset = getOffset(position, positionOffset);
+        int width = imageView.getWidth();
+        outgoing.setTranslationX(-offset * (FACTOR * width));
+        imageView.setTranslationX((1 - offset) * (FACTOR * width));
         imageView.setAlpha(offset);
     }
 
     public void backwards(int position, float positionOffset) {
         float offset = getOffset(position, positionOffset);
+        int width = imageView.getWidth();
+        outgoing.setTranslationX((1 - offset) * (FACTOR * width));
+        imageView.setTranslationX(-(offset) * (FACTOR * width));
         imageView.setAlpha(1 - offset);
     }
+
 
     private float getOffset(int position, float positionOffset) {
         int positions = position - start;
